50737ed5d2113d990ebde80cc347f14e5b4e2cdc
Lucas van Staden <sales@proxiblue.com.au>
2022-04-02 00:29:39 +0000

    Generate a fresh formkey at result build

--- Plugin/ItemFactory.php
+++ Plugin/ItemFactory.php
@@ -3,38 +3,59 @@
 namespace ProxiBlue\HyvaSmileAutocompleteAddToCart\Plugin;
 
 use Magento\Checkout\Helper\Cart as CartHelper;
+use Magento\Framework\Data\Form\FormKey;
+
+// phpcs:disable Generic.Files.LineLength.TooLong
 
 class ItemFactory
 {
 
     /**
+     * Cart helper to get add to cart url
+     *
      * @var CartHelper
      */
     protected $cartHelper;
 
     /**
+     * Generate an up-to-date form key
+     *
+     * @var FormKey
+     */
+    protected $formKey;
+
+    /**
+     * Constructor
+     *
      * @param CartHelper $cartHelper
+     * @param FormKey $formKey
      */
     public function __construct(
-        CartHelper $cartHelper
+        CartHelper $cartHelper,
+        FormKey    $formKey
     ) {
-        $this->cartHelper             = $cartHelper;
+        $this->cartHelper = $cartHelper;
+        $this->formKey = $formKey;
     }
 
     /**
      * Inject the add to cart url here
      *
-     * @param \Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject
-     * @param callable $proceed
-     * @param $data
-     * @return mixed
+     * @param \Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject Object
+     * @param array                                                             $data    Array of values
+     *
+     * @return array
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
-    public function beforeCreate(\Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject, $data)
+    public function beforeCreate(\Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject, array $data)
     {
         if ($data['product']
             && $data['product']->isSaleable()
-            && $data['product']->getTypeId() == \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE) {
-            $data['add_url'] = $this->cartHelper->getAddUrl($data['product'], ['useUencPlaceholder'=>true]);
+            && $data['product']->getTypeId() == \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE
+        ) {
+            $data['add_url'] = $this->cartHelper->getAddUrl($data['product'], ['useUencPlaceholder' => true]);
+            $data['formkey'] = $this->formKey->getFormKey();
             $data['type'] = 'product_add_to_cart';
         }
 
