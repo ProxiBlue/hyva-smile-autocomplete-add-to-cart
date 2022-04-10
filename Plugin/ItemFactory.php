879fbda7afec05e70b2aff4a574e5f80b420cdf7
Lucas van Staden <sales@proxiblue.com.au>
2022-04-10 05:24:27 +0000

    Add in QTY. Cleanup responsive. Ability to return back to current page.

--- Plugin/ItemFactory.php
+++ Plugin/ItemFactory.php
@@ -2,8 +2,11 @@
 
 namespace ProxiBlue\HyvaSmileAutocompleteAddToCart\Plugin;
 
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\Product;
 use Magento\Checkout\Helper\Cart as CartHelper;
 use Magento\Framework\Data\Form\FormKey;
+use Magento\CatalogInventory\Model\Stock\StockItemRepository;
 
 // phpcs:disable Generic.Files.LineLength.TooLong
 
@@ -25,24 +28,39 @@ class ItemFactory
     protected $formKey;
 
     /**
+     * @var StockItemRepository
+     */
+    private $stockItemRepository;
+
+    /**
+     * Cached stock data
+     *
+     * @var null
+     */
+    private $stockItem = null;
+
+    /**
      * Constructor
      *
      * @param CartHelper $cartHelper
      * @param FormKey $formKey
      */
     public function __construct(
-        CartHelper $cartHelper,
-        FormKey    $formKey
-    ) {
+        CartHelper          $cartHelper,
+        FormKey             $formKey,
+        StockItemRepository $stockItemRepository
+    )
+    {
         $this->cartHelper = $cartHelper;
         $this->formKey = $formKey;
+        $this->stockItemRepository = $stockItemRepository;
     }
 
     /**
      * Inject the add to cart url here
      *
      * @param \Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject Object
-     * @param array                                                             $data    Array of values
+     * @param array $data Array of values
      *
      * @return array
      *
@@ -54,11 +72,133 @@ class ItemFactory
             && $data['product']->isSaleable()
             && $data['product']->getTypeId() == \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE
         ) {
+            $data['qty_min'] = $this->getMinQty($data['product']);
+            $data['qty_max'] = $this->getMaxQty($data['product']);
+            $data['qty_increments_enabled'] = $this->areQtyIncrementsEnabled($data['product']);
+            $data['qty_increments'] = $this->getQtyIncrements($data['product']);
+            $data['qty_len'] = $this->getMaxQtyLength($data['product']);
+            $data['qty_step'] = $this->getQtyStep($data['product']);
+            $data['qty_format'] = $this->getQtyFormat($data['product']);
             $data['add_url'] = $this->cartHelper->getAddUrl($data['product'], ['useUencPlaceholder' => true]);
             $data['formkey'] = $this->formKey->getFormKey();
             $data['type'] = 'product_add_to_cart';
+            $data['form_name'] = "ac_addtocart_" . $data['product']->getId();
         }
 
         return [$data];
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return \Magento\CatalogInventory\Api\Data\StockItemInterface|void
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getStockItem(ProductInterface $product)
+    {
+        if (empty($this->stockItem)) {
+            $this->stockItem = $this->stockItemRepository->get($product->getId());
+        }
+        return $this->stockItem;
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return float|int|null
+     */
+    public function getMinQty(ProductInterface $product)
+    {
+        if (!($stockItem = $this->getStockItem($product))) {
+            return 1;
+        }
+        $min = $stockItem->getMinSaleQty();
+        return isset($min)
+            ? ($stockItem->getIsQtyDecimal() ? (float)$min : (int)$min)
+            : 1;
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return float|int|null
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getMaxQty(ProductInterface $product)
+    {
+        if (!($stockItem = $this->getStockItem($product))) {
+            return 999999;
+        }
+        $max = $stockItem->getMaxSaleQty();
+        return isset($max)
+            ? ($stockItem->getIsQtyDecimal() ? (float)$max : (int)$max)
+            : 999999;
+    }
+
+    /**
+     * @param ProductInterface|Product $product
+     * @return bool
+     */
+    public function areQtyIncrementsEnabled(ProductInterface $product): bool
+    {
+        return (($stockItem = $this->getStockItem($product))) && $stockItem->getEnableQtyIncrements();
+    }
+
+    /**
+     * @param ProductInterface|Product $product
+     * @return false|float
+     */
+    public function getQtyIncrements(ProductInterface $product)
+    {
+        if (!($stockItem = $this->getStockItem($product))) {
+            return 1;
+        }
+        return $stockItem->getQtyIncrements() ? $stockItem->getQtyIncrements() : 1;
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return int
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getMaxQtyLength(productInterface $product)
+    {
+        $maxQty = $this->getMaxQty($product);
+        return ($maxQty ? strlen((string)$maxQty) : 4)
+            + (/* add one if decimal for separator */
+            (int)$this->isQtyDecimal($product));
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return bool
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function isQtyDecimal(ProductInterface $product): bool
+    {
+        return (($stockItem = $this->getStockItem($product))) && $stockItem->getIsQtyDecimal();
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return float|null
+     */
+    public function getQtyStep(productInterface $product)
+    {
+        $qtyIncrements = $this->getQtyIncrements($product);
+        return $qtyIncrements ? $qtyIncrements : 1;
+    }
+
+    public function getQtyFormat(productInterface $product)
+    {
+        if ($this->isQtyDecimal($product)) {
+            $result = ['type' => "text",
+                'pattern' => "[0-9](\.[0-9])?{0," . $this->getMaxQty($product) . "}'",
+                'inputmode' => "decimal"
+            ];
+        } else {
+            $result = ['type' => "number",
+                'pattern' => "[0-9]{0," . $this->getMaxQty($product) . "}",
+                'inputmode' => "numeric"
+            ];
+        }
+        return $result;
     }
 }
